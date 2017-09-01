ae6aaa17868c3c7c0669bce1d5b0809d5417face
Pawel Wisniewski <wisniewski@bury.com>
2017-09-01 09:35:15 +0200

    remove thermostat feature from cgi_sensors.c

--- user/cgi/cgi_sensors.c
+++ user/cgi/cgi_sensors.c
@@ -31,26 +31,6 @@ int ICACHE_FLASH_ATTR cgiSensorSettings(HttpdConnData *connData) {
 	len=httpdFindArg(connData->post->buff, "sensor-bmp180-enable", buff, sizeof(buff));
 	sysCfg.sensor_bmp180_enable = (len > 0) ? 1:0;
 
-	len=httpdFindArg(connData->post->buff, "thermostat1-input", buff, sizeof(buff));
-	if (len>0) {
-		sysCfg.thermostat1_input=atoi(buff);
-	}
-
-	len=httpdFindArg(connData->post->buff, "thermostat1-input", buff, sizeof(buff));
-	if (len>0) {
-		sysCfg.thermostat1_input=atoi(buff);
-	}
-
-	len=httpdFindArg(connData->post->buff, "thermostat1hysteresishigh", buff, sizeof(buff));
-	if (len>0) {
-		sysCfg.thermostat1hysteresishigh=atoi(buff);
-	}
-
-	len=httpdFindArg(connData->post->buff, "thermostat1hysteresislow", buff, sizeof(buff));
-	if (len>0) {
-		sysCfg.thermostat1hysteresislow=atoi(buff);
-	}
-
 	CFG_Save();
 	httpdRedirect(connData, "/");
 	return HTTPD_CGI_DONE;
@@ -72,38 +52,6 @@ void ICACHE_FLASH_ATTR tplSensorSettings(HttpdConnData *connData, char *token, v
 
 	if (os_strcmp(token, "sensor-bmp180-enable")==0) {
 		os_strcpy(buff, sysCfg.sensor_bmp180_enable == 1 ? "checked" : "" );
-	}
-
-	if (os_strcmp(token, "selectedds18b20")==0) {
-		os_strcpy(buff, sysCfg.thermostat1_input == 0 ? "selected" : "" );
-	}
-
-	if (os_strcmp(token, "selecteddht22t")==0) {
-		os_strcpy(buff, sysCfg.thermostat1_input == 1 ? "selected" : "" );
-	}
-
-	if (os_strcmp(token, "selecteddht22h")==0) {
-		os_strcpy(buff, sysCfg.thermostat1_input == 2 ? "selected" : "" );
-	}
-
-	if (os_strcmp(token, "selectedmqtt")==0) {
-		os_strcpy(buff, sysCfg.thermostat1_input == 3 ? "selected" : "" );
-	}
-
-	if (os_strcmp(token, "selectedserial")==0) {
-		os_strcpy(buff, sysCfg.thermostat1_input == 4 ? "selected" : "" );
-	}
-
-	if (os_strcmp(token, "selectedfixed")==0) {
-		os_strcpy(buff, sysCfg.thermostat1_input == 5 ? "selected" : "" );
-	}
-
-	if (os_strcmp(token, "thermostat1hysteresishigh")==0) {
-		os_sprintf(buff,"%d", (int)sysCfg.thermostat1hysteresishigh);
-	}
-
-	if (os_strcmp(token, "thermostat1hysteresislow")==0) {
-		os_sprintf(buff,"%d", (int)sysCfg.thermostat1hysteresislow);
 	}
 
 	httpdSend(connData, buff, -1);
