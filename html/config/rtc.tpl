954affc098eea17a2695b3716d0b689e0294c17f
Pawel Wisniewski <wisniewski@bury.com>
2017-09-01 09:52:50 +0200

    improve/bugfix html-rtc interface

--- html/config/rtc.tpl
+++ html/config/rtc.tpl
@@ -2,15 +2,16 @@
 	<head><title>Real Time Clock Settings</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="../style.css">
 	</head>
 	<body>
 		<div id="main">
 			<h1>Adjust Date & Time</h1>
-			<form method="post" action="rtc.cgi">
-				Time <input type="time" value="%rtc_time%"><br/>
-				Date <input type="date" value="%rtc_date%"> 
-				<input type="submit" name="submit" value="Submit New Settings">
+			<form name="rtcform" method="post" action="rtc.cgi">
+				Time <input type="time" name="time" id="time" value="%rtc_time%"><br/>
+				Date <input type="date" name="date" id="date" value="%rtc_date%">
+				<button type="button" onClick="parent.location='/'">Back</button>
+				<input type="submit" name="save" value="Save">
 			</form>
 		</div>
 	</body>
--- user/cgi/cgi_rtc.c
+++ user/cgi/cgi_rtc.c
@@ -14,10 +14,14 @@ void ICACHE_FLASH_ATTR tplRTC(HttpdConnData *connData, char *token, void **arg) 
 	os_strcpy(buff, "Unknown");
 
 	if( ds1307_getTime(&tm) ) {
+		os_printf( "RTC: date %d/%02d/%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec );
+
 		if (os_strcmp(token, "rtc_date")==0) {
-			os_sprintf(buff, "%d/%d/%d", 1900 + tm.tm_year, tm.tm_mon + 1, tm.tm_mday );
+			os_printf( "--> RTC_DATE\n" );
+			os_sprintf(buff, "%d-%d-%d", 1900 + tm.tm_year, tm.tm_mon + 1, tm.tm_mday );
 		}
 		if (os_strcmp(token, "rtc_time")==0) {
+			os_printf( "--> RTC_TIME\n" );
 			os_sprintf(buff, "%d:%d", tm.tm_hour, tm.tm_min );
 		}
 	}
@@ -28,18 +32,30 @@ int ICACHE_FLASH_ATTR cgiRTC(HttpdConnData *connData) {
 extern int v_sscanf(const char *str, const char *format, ...);
 	char buff[256];
 	struct tm tm;
+	int c_ok = 0;
 
 	if(connData->conn == NULL)
 		//Connection aborted. Clean up.
 		return HTTPD_CGI_DONE;
 
-	if( httpdFindArg(connData->post->buff, "time", buff, sizeof(buff)) > 0 )
-		v_sscanf( buff, "%d:%d:%d", &tm.tm_hour, &tm.tm_min, &tm.tm_sec );
+	if( httpdFindArg(connData->post->buff, "time", buff, sizeof(buff)) > 0 ){
+		tm.tm_sec = 0;
+		os_printf( "TIME %s\n", buff );
+		c_ok += v_sscanf( buff, "%d:%d", &tm.tm_hour, &tm.tm_min );
+	}
 
-	if( httpdFindArg(connData->post->buff, "date", buff, sizeof(buff)) > 0 )
-		v_sscanf( buff, "%d/%d/%d", &tm.tm_year, &tm.tm_mon, &tm.tm_mday );
+	if( httpdFindArg(connData->post->buff, "date", buff, sizeof(buff)) > 0 ){
+		os_printf( "DATA %s\n", buff );
+		c_ok += v_sscanf( buff, "%d-%d-%d", (int *)&tm.tm_year, &tm.tm_mon, &tm.tm_mday );
+		os_printf("--> %d %d %d", tm.tm_year, tm.tm_mon, tm.tm_mday );
+	}
 
-	ds1307_setTime( &tm );
+	if( c_ok == 5 ){
+		tm.tm_year = tm.tm_year - 1900;
+		tm.tm_mon = tm.tm_mon - 1;
+		tm.tm_sec = 0;
+		ds1307_setTime( &tm );
+	}
 	httpdRedirect(connData, "/");
 	return HTTPD_CGI_DONE;
 }
